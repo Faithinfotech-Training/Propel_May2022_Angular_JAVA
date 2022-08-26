5b25756c32129083a95302f16955dd926bcdcf9e
vivekgopal12 <vivekgopal98@gmail.com>
2022-08-26 10:30:33 +0530

    first push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component : LoginComponent },
+  { path: 'app-doctor', component : DoctorComponent }
+
+
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
